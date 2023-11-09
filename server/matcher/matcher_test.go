4cf849bad3b2f38be3dfc16e1d43ed3c55395707
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2023-11-09 10:50:58 -0300

    Separate tests in lobby size assert, and one lobby for connection test.

--- server/matcher/matcher_test.go
+++ server/matcher/matcher_test.go
@@ -5,36 +5,72 @@ import (
 	"io"
 	"net"
 	"slices"
+	"sync"
 	"testing"
 )
 
 const (
-	dummyMessage = "dummy"
-	maxLobbySize = 2
+	dummyMessage   = "dummy"
+	maxLobbySize   = 2
+	maxLobbyAmount = 5
 )
 
-func broadcastDummyMessage(connections ...net.Conn) {
-	for _, c := range connections {
-		go func(c net.Conn) {
-			io.WriteString(c, dummyMessage)
-			c.Close()
-		}(c)
+func createPipeArray(lobbySize int) [][2]net.Conn {
+	pipes := make([][2]net.Conn, lobbySize)
+	for i := range pipes {
+		pipes[i][0], pipes[i][1] = net.Pipe()
 	}
+	return pipes
 }
 
-func TestLobbyConnectsUsers(t *testing.T) {
+// This tests creates pipes for a number of lobbies with fixed size and matches them, with a
+// game starter function that asserts the expected lobby size.
+//
+// This proves that all lobbies have the requested size.
+func TestLobbySizeIsCorrect(t *testing.T) {
 	for lobbySize := 2; lobbySize <= maxLobbySize; lobbySize++ {
-		pipes := make([][2]net.Conn, lobbySize)
-		for i := range pipes {
-			pipes[i][0], pipes[i][1] = net.Pipe()
-		}
+		pipes := createPipeArray(lobbySize * maxLobbyAmount)
+
+		wg := sync.WaitGroup{}
+		wg.Add(maxLobbyAmount)
 
 		starter := func(connections ...net.Conn) {
 			if lobbySize != len(connections) {
 				t.Errorf("Expected lobby size of %v, but got %v", lobbySize, len(connections))
 			}
 
-			broadcastDummyMessage(connections...)
+			wg.Done()
+		}
+
+		matcher := matcher.NewMatcher(lobbySize, matcher.MatchStarterFunc(starter))
+
+		for _, c := range pipes {
+			go matcher.Match(c[1])
+		}
+
+		wg.Wait()
+	}
+}
+
+// This tests creates pipes for a number of lobbies with fixed size and matches them,
+// with a game starter function that broadcast a dummy message to all connections.
+// Each remote connection should read the correct message exactly once.
+//
+// This proves that a connections is not involved in more than one match.
+func TestLobbyConnectsAllUsers(t *testing.T) {
+	for lobbySize := 2; lobbySize <= maxLobbySize; lobbySize++ {
+		pipes := createPipeArray(lobbySize * maxLobbyAmount)
+
+		starter := func(connections ...net.Conn) {
+			for _, c := range connections {
+				go func(c net.Conn) {
+					_, err := io.WriteString(c, dummyMessage)
+					if err != nil {
+						t.Errorf("Two matches where started with the same connection")
+					}
+					c.Close()
+				}(c)
+			}
 		}
 
 		matcher := matcher.NewMatcher(lobbySize, matcher.MatchStarterFunc(starter))
