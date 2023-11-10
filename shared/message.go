5809171f8615d44c2ae8f2803b27ba37cc563c8e
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2023-11-10 19:23:55 -0300

    Message fields should be exported

--- shared/message.go
+++ shared/message.go
@@ -2,13 +2,13 @@ package shared
 
 // Server to client, notifies the start of a round.
 type Round struct {
-	character rune
+	Character rune
 }
 
 // Client to server, asks the server to stop the round, or
 // sends the user's input if the round had already ended.
 type StopRequest struct {
-	words map[Category]string
+	Words map[Category]string
 }
 
 // Server to client, notifies all the other clients
@@ -18,7 +18,7 @@ type StopNotify struct{}
 // Server to client, informs all users the result
 // of the last round.
 type Result struct {
-	scores map[string]int
+	Scores map[string]int
 }
 
 type Category int
