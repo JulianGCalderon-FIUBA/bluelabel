856a671e39d0aa59a1939c4a9ac3f4e3f5f9e864
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2023-11-14 17:04:19 -0300

    simpler test for matcher (too complex)

--- server/matcher/matcher_test.go
+++ server/matcher/matcher_test.go
@@ -2,17 +2,8 @@ package matcher_test
 
 import (
 	"bluelabel/server/matcher"
-	"io"
 	"net"
-	"slices"
-	"sync"
 	"testing"
-)
-
-const (
-	dummyMessage   = "dummy"
-	maxLobbySize   = 8
-	maxLobbyAmount = 8
 )
 
 func createPipeArray(lobbySize int) [][2]net.Conn {
@@ -23,71 +14,21 @@ func createPipeArray(lobbySize int) [][2]net.Conn {
 	return pipes
 }
 
-// This tests creates pipes for a number of lobbies with fixed size and matches them, with a
-// game starter function that asserts the expected lobby size.
-//
-// This proves that all lobbies have the requested size.
 func TestLobbySizeIsCorrect(t *testing.T) {
-	for lobbySize := 2; lobbySize <= maxLobbySize; lobbySize++ {
-		pipes := createPipeArray(lobbySize * maxLobbyAmount)
+	lobbySize := 8
+	pipes := createPipeArray(lobbySize)
 
-		lobbyWaitGroup := sync.WaitGroup{}
-		lobbyWaitGroup.Add(maxLobbyAmount)
-
-		starter := func(connections ...net.Conn) {
-			if lobbySize != len(connections) {
-				t.Errorf("Expected lobby size of %v, but got %v", lobbySize, len(connections))
-			}
-
-			lobbyWaitGroup.Done()
-		}
-
-		matcher := matcher.NewMatcher(lobbySize, matcher.MatchStarterFunc(starter))
-
-		for _, c := range pipes {
-			matcher.Match(c[1])
-		}
-
-		lobbyWaitGroup.Wait()
+	ch := make(chan int)
+	starter := func(connections ...net.Conn) {
+		ch <- len(connections)
 	}
-}
+	matcher := matcher.NewMatcher(lobbySize, matcher.MatchStarterFunc(starter))
+	for _, c := range pipes {
+		matcher.Match(c[1])
+	}
 
-// This tests creates pipes for a number of lobbies with fixed size and matches them,
-// with a game starter function that broadcast a dummy message to all connections.
-// Each remote connection should read the correct message exactly once.
-//
-// This proves that a connections is not involved in more than one match.
-func TestLobbyConnectsAllUsers(t *testing.T) {
-	for lobbySize := 2; lobbySize <= maxLobbySize; lobbySize++ {
-		pipes := createPipeArray(lobbySize * maxLobbyAmount)
-
-		starter := func(connections ...net.Conn) {
-			for _, c := range connections {
-				go func(c net.Conn) {
-					_, err := io.WriteString(c, dummyMessage)
-					if err != nil {
-						t.Errorf("Two matches where started with the same connection")
-					}
-					c.Close()
-				}(c)
-			}
-		}
-
-		matcher := matcher.NewMatcher(lobbySize, matcher.MatchStarterFunc(starter))
-
-		for _, c := range pipes {
-			go matcher.Match(c[1])
-		}
-
-		for _, c := range pipes {
-			buf, err := io.ReadAll(c[0])
-			if err != nil {
-				t.Errorf("Could not read from pipe")
-			}
-
-			if !slices.Equal(buf, []byte(dummyMessage)) {
-				t.Errorf("Expected %s from remote pipe, but read %s", dummyMessage, buf)
-			}
-		}
+	actualLobbySize := <-ch
+	if lobbySize != actualLobbySize {
+		t.Errorf("Expected lobby size of %v, but got %v", lobbySize, actualLobbySize)
 	}
 }
