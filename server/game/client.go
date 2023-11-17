24e20bc2b271bf521243a848008cbcff5d90d092
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2023-11-17 20:41:00 -0300

    Doc for receive message in client and remove test

--- server/game/client.go
+++ server/game/client.go
@@ -22,17 +22,12 @@ func makeClient(connection net.Conn) client {
 	}
 }
 
-// func (c *client) loop() {
-// 	for {
-// 		message := c.receive()
-// 	}
-// }
-
-// Sends a gob-enconded structure to the client, as an interface.
+// Sends a gob-enconded structure to the client
 func (c *client) send(structure any) error {
 	return c.encoder.Encode(structure)
 }
 
+// Sends a gob-enconded interface from the client
 func (c *client) receive() (message any) {
 	c.decoder.Decode(&message)
 	return
